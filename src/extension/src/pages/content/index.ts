3de06c54d12ca44c2ff3f6283f2f9cf86378b34e
Devon Crebbin <devon@land.org.au>
2025-02-16 18:28:06 +0000

    added conversation observer for chat monitoring

--- src/extension/src/pages/content/index.ts
+++ src/extension/src/pages/content/index.ts
@@ -18,6 +18,9 @@ let widthCropping = isFullScreen ? 1150 : 300;
 let verticalCropping = isFullScreen ? 200 : 100;
 // Whether the screenshot button sends the image to the server or downloads it
 let screenshotButtonSendsToServer = true;
+let conversationName = "Perplexity";
+const onTheCallScreen = document.location.href.includes("groupcall/ROOM");
+const onTheConversationScreen = document.location.href.includes("messages/t/");
 
 const cameraOnIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
@@ -27,6 +30,14 @@ const cameraOffIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewB
   <path stroke-linecap="round" stroke-linejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M12 18.75H4.5a2.25 2.25 0 0 1-2.25-2.25V9m12.841 9.091L16.5 19.5m-1.409-1.409c.407-.407.659-.97.659-1.591v-9a2.25 2.25 0 0 0-2.25-2.25h-9c-.621 0-1.184.252-1.591.659m12.182 12.182L2.909 5.909M1.5 4.5l1.409 1.409" />
 </svg>
 `;
+
+const chatOnIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
+</svg>`;
+
+const chatOffIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+</svg>`;
 
 const screenshotIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
@@ -92,17 +103,64 @@ const takeScreenshotButton = createButton(
   screenshotIcon
 );
 
-const monitoringButton = createButton(
-  "Start Monitoring",
+const videoMonitoringButton = createButton(
+  "Start Video Monitoring",
   "monitoring-button",
   cameraOffIcon
 );
 
-monitoringButton.addEventListener("click", () => {
+const chatMonitoringButton = createButton(
+  "Start Chat Monitoring",
+  "chat-monitoring-button",
+  chatOffIcon
+);
+
+let chatObserver: MutationObserver | null = null;
+
+chatMonitoringButton.addEventListener("click", () => {
   if (isMonitoring) {
     isMonitoring = false;
-    monitoringButton.children[0].innerHTML = "Start Monitoring";
-    monitoringButton.children[1].innerHTML = cameraOffIcon;
+    chatMonitoringButton.children[0].innerHTML = "Start Chat Monitoring";
+    chatMonitoringButton.children[1].innerHTML = chatOffIcon;
+    if (chatObserver) {
+      chatObserver.disconnect();
+      chatObserver = null;
+    }
+  } else {
+    isMonitoring = true;
+    chatMonitoringButton.children[0].innerHTML = "Stop Chat Monitoring";
+    chatMonitoringButton.children[1].innerHTML = chatOnIcon;
+    const chat = document.querySelector(
+      `div[aria-label='Messages in conversation titled ${conversationName}']`
+    );
+    if (chat) {
+      chatObserver = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+          if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+            const div = mutation.addedNodes[0] as HTMLDivElement;
+            const messageContainer = div.querySelector("div.html-div");
+            if (messageContainer && messageContainer?.childNodes?.length > 3) {
+              const receivedMessage = messageContainer.textContent;
+              if (receivedMessage) {
+                sendLog({
+                  content: receivedMessage,
+                  timeReceived: new Date().toISOString(),
+                });
+              }
+            }
+          }
+        });
+      });
+      chatObserver.observe(chat, { childList: true, subtree: true });
+    }
+  }
+});
+
+videoMonitoringButton.addEventListener("click", () => {
+  if (isMonitoring) {
+    isMonitoring = false;
+    videoMonitoringButton.children[0].innerHTML = "Start Video Monitoring";
+    videoMonitoringButton.children[1].innerHTML = cameraOffIcon;
     enableButton(takeScreenshotButton);
     if (intervalId) {
       clearInterval(intervalId);
@@ -110,8 +168,8 @@ monitoringButton.addEventListener("click", () => {
     }
   } else {
     isMonitoring = true;
-    monitoringButton.children[0].innerHTML = "Stop Monitoring";
-    monitoringButton.children[1].innerHTML = cameraOnIcon;
+    videoMonitoringButton.children[0].innerHTML = "Stop Video Monitoring";
+    videoMonitoringButton.children[1].innerHTML = cameraOnIcon;
     disableButton(takeScreenshotButton);
     intervalId = setInterval(() => {
       takeAndSendScreenshot();
@@ -193,7 +251,7 @@ async function requestDisplayPermission() {
     requestPermissionButton.style.backgroundColor = "green";
     requestPermissionButton.children[1].innerHTML = tickIcon;
     enableButton(takeScreenshotButton);
-    enableButton(monitoringButton);
+    enableButton(videoMonitoringButton);
   }
 }
 
@@ -312,19 +370,46 @@ function createMainContainer() {
   return container;
 }
 
+function createConversationNameInput() {
+  const input = document.createElement("input");
+  input.type = "text";
+  input.value = conversationName;
+  input.id = "conversation-name-input";
+  input.placeholder = "Conversation Name";
+  input.style.backgroundColor = "#474747";
+  input.style.color = "white";
+  input.style.fontSize = "14px";
+  input.style.fontWeight = "bold";
+  input.style.borderRadius = "5px";
+  input.style.border = "none";
+  input.style.padding = "10px";
+  input.style.width = "200px";
+  input.addEventListener("change", (e) => {
+    const value = (e.target as HTMLInputElement).value;
+    conversationName = value;
+  });
+  return input;
+}
+
 function addCustomUI() {
   const mainContainer = createMainContainer();
-  const intervalContainer = createMonitoringIntervalContainer();
-  mainContainer.appendChild(intervalContainer);
-  mainContainer.appendChild(monitoringButton);
-  mainContainer.appendChild(takeScreenshotButton);
-  mainContainer.appendChild(requestPermissionButton);
+  if (onTheConversationScreen || onTheCallScreen) {
+    mainContainer.appendChild(createConversationNameInput());
+    mainContainer.appendChild(chatMonitoringButton);
+  }
+  if (onTheCallScreen) {
+    const intervalContainer = createMonitoringIntervalContainer();
+    mainContainer.appendChild(intervalContainer);
+    mainContainer.appendChild(videoMonitoringButton);
+    mainContainer.appendChild(takeScreenshotButton);
+    mainContainer.appendChild(requestPermissionButton);
+  }
   document.body.appendChild(mainContainer);
 }
 
 function init() {
   disableButton(takeScreenshotButton);
-  disableButton(monitoringButton);
+  disableButton(videoMonitoringButton);
   addCustomUI();
 }
 
