24a47aebf5bd3ba5ee19cb099b9f2c57f4448760
Devon Crebbin <devon@land.org.au>
2025-06-08 00:28:28 +0100

    updated popup ux

--- src/app/popup/main.tsx
+++ src/app/popup/main.tsx
@@ -10,7 +10,6 @@ import { Provider, TTSProvider } from "~/types";
 
 const Popup = () => {
   const [activeTab, setActiveTab] = useState("logs");
-
   function popOut() {
     chrome.windows.create({
       url: chrome.runtime.getURL("popup.html"),
@@ -21,9 +20,15 @@ const Popup = () => {
     window.close();
   }
 
+  const isWindowedPopup = window.outerWidth > 400 && window.outerHeight > 400;
+
   return (
     <Layout>
-      <div className="w-screen max-h-screen flex flex-col items-center justify-baseline h-96 text-white font-sans p-4">
+      <div
+        className={`w-screen max-h-screen flex flex-col items-center justify-baseline text-white font-sans p-4 ${
+          isWindowedPopup ? "h-full" : "h-96"
+        }`}
+      >
         <div className="flex flex-row w-full justify-between gap-2.5 px-8 py-2">
           <button
             className={`text-sm flex items-center gap-2 cursor-pointer ${
@@ -48,25 +53,27 @@ const Popup = () => {
         {activeTab === "logs" && <Logs />}
         {activeTab === "settings" && <Settings />}
 
-        <button
-          className="absolute bottom-0 m-2 right-0 w-fit p-2 bg-[#4a4a4a] border-none rounded text-white cursor-pointer hover:bg-[#5a5a5a]"
-          onClick={popOut}
-        >
-          <svg
-            className="w-6 h-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
+        {!isWindowedPopup && (
+          <button
+            className="absolute bottom-0 m-2 right-0 w-fit p-2 bg-[#4a4a4a] border-none rounded text-white cursor-pointer hover:bg-[#5a5a5a]"
+            onClick={popOut}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-            />
-          </svg>
-        </button>
+            <svg
+              className="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+              />
+            </svg>
+          </button>
+        )}
       </div>
     </Layout>
   );
@@ -104,7 +111,7 @@ const Logs = () => {
       id="logs-container"
     >
       <button
-        className="w-fit absolute bottom-0 left-0 m-2 p-2 bg-[#4a4a4a] border-none rounded text-white cursor-pointer hover:bg-[#5a5a5a]"
+        className="w-fit fixed bottom-0 left-0 m-2 p-2 bg-[#4a4a4a] border-none rounded text-white cursor-pointer hover:bg-[#5a5a5a]"
         onClick={() => {
           storage.set([]);
           setReceivedLogs([]);
